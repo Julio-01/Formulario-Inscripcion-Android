41b2823c838f4275183fc60572389de6a9f5e8b6
Julio Torres <julio_escobedo_01@ciencias.unam.mx>
2024-04-05 11:28:08 -0600

    Creacion interfaz de inscripcion

---
+++ app/src/main/java/com/example/interfaz/Interfaz.java
@@ -0,0 +1,13 @@
+package com.example.interfaz;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Interfaz extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+    }
+}
