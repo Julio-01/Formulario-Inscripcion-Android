92dda0b9dd263f5aa8a1114aa3b63793a8a720b8
Julio Torres <julio_escobedo_01@ciencias.unam.mx>
2024-04-26 12:37:00 -0600

    Creacion de menu derecho con actionBar

--- app/src/main/java/com/example/interfaz/Interfaz.java
+++ app/src/main/java/com/example/interfaz/Interfaz.java
@@ -2,12 +2,42 @@ package com.example.interfaz;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class Interfaz extends Activity{
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.second_layout);
+        setContentView(R.layout.main_layout);
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        int itemId = item.getItemId();
+        if (itemId == R.id.action_new) {
+            Log.d("HomeActivity", (String) "action_new");
+            return true;
+        } else if (itemId == R.id.action_delete) {
+            Log.d("HomeActivity", (String) "action_delete");
+            return true;
+        } else if (itemId == R.id.action_empty) {
+            Log.d("HomeActivity", (String) "action_empty");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
